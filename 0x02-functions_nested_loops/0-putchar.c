93557082506483559174568015659f623832b606
abdulrhman <abdulrhman151@gmail.com>
2022-10-18 04:06:08 -0700

    My first Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-
-/**
- * main - Entry point
- *
- * Description: write a program that prints "_putchar" followed by a new line.
- *
- * Return: zero
- */
+*/
 int main(void)
 {
 	_putchar('_');
