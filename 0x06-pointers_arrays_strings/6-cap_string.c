75d9964723a1554ff94bf82e99af5497c84ebdc3
abdulrhman25 <you@example.com>
2022-10-26 13:55:37 -0700

    add cap_string file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isLower - determines whether ascii is lowercase
+ *
+ * @c: character
+ *
+ * Return: 1 if true, 0 if false
+ */
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
