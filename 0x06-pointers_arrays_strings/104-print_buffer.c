bc4ede7ebb6146903677ed4fcabfaa9ad8ec3ee8
abdulrhman25 <you@example.com>
2022-10-26 14:12:15 -0700

    print_buffer file

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -62,3 +62,29 @@ void printASCII(char *b, int start, int end)
 		i++;
 	}
 }
+
+/**
+ * print_buffer - prints a buffer
+ *
+ * @b: string
+ *
+ * @size: size of buffer
+ */
+void print_buffer(char *b, int size)
+{
+	int start, end;
+
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+	}
+	else
+		printf("\n");
+}
