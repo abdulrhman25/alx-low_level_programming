3b4a83706d1aeeab43f1174cb83c8aa79afa2731
abdulrhman <abdulrhman151@gmail.com>
2022-11-07 04:58:19 -0800

    adding README && Create_array && main.h

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 /**
- *  * create_array - creates an array of chars,
- *   * @size: size of array
- *    * @c: char to fill array with
- *     * Return: string of chars
- *      */
+ * create_array - creates an array of chars,
+ * @size: size of array
+ * @c: char to fill array with
+ * Return: string of chars
+ */
 char *create_array(unsigned int size, char c)
 {
-		unsigned int i;
-			char *ptr;
+	unsigned int i;
+	char *ptr;
 
-				ptr = malloc(size * sizeof(char));
-					if (size == 0 || !ptr)
-								return ('\0');
-						for (i = 0; i < size; i++)
-									ptr[i] = c;
-							return (ptr);
+	ptr = malloc(size * sizeof(char));
+	if (size == 0 || !ptr)
+		return ('\0');
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
 }
 
